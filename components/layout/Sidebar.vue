89e51e8277dde145c94c60b229a915a18abfd20c
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-11-06 07:15:44 +0100

    conficuration for tailfind was finished

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -3,7 +3,7 @@
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
     <NuxtLink to="/" class="mb-10 block">
-      <NuxtImg width="100px" src="logo.svg" class="mx-auto" />
+      <NuxtImg width="100px" src="/favicon.ico" class="mx-auto" />
     </NuxtLink>
   </aside>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss']
-})
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
+  ],
+  shadcn: {
+    prefix: "",
+
+    componentDir: "./components/ui",
+  },
+});
