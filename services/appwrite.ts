121d214964a454361adbd3e66c2ff456ef1de5c1
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-11-21 22:56:17 +0100

    check for errors 5

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,4 +1,3 @@
-// src/services/appwrite.js
 import { Client, Account, Databases, Storage } from "appwrite";
 
 // Initialize the client
@@ -6,8 +5,8 @@ const client = new Client();
 
 // Set endpoint and project ID using environment variables
 client
-  .setEndpoint(process.env.VUE_APP_APPWRITE_ENDPOINT) // Your Appwrite API endpoint
-  .setProject(process.env.VUE_APP_APPWRITE_PROJECT_ID); // Your Appwrite Project ID
+  .setEndpoint(process.env.VUE_APP_APPWRITE_ENDPOINT || "") // Your Appwrite API endpoint
+  .setProject(process.env.VUE_APP_APPWRITE_PROJECT_ID || ""); // Your Appwrite Project ID
 
 // Initialize Appwrite services (you can use these services to interact with Appwrite)
 const account = new Account(client);
